528e97632a77bab1944fd3d5c185dccf8eaff453
xuzhaoyang <15809246338@163.com>
2023-05-01 21:22:24 +0800

    初始化

---
+++ test1.py
@@ -0,0 +1,14 @@
+'''
+Author: xuzhaoyang
+Date: 2023-04-09 19:27:24
+LastEditors: xuzhaoyang
+LastEditTime: 2023-05-01 20:34:22
+FilePath: /VSCode_Python/test1.py
+
+'''
+# 测试
+import sys
+import torch
+print(sys.version)
+print(sys.executable)
+print(torch)
